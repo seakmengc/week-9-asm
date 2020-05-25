b67f407c78bc41ce133501ca88330ddd027c6867
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-05-25 10:15:18 +0700

    finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('sections.client')
+@include('sections.client')
 
-    @include('sections.about_us')
-    
-    @include('sections.why_us')
+@include('sections.about_us')
 
-    @include('sections.skills')
-    
-    @include('sections.services')
-    
-    @include('sections.cta')
-    
-    @include('sections.portfolio')
-    
-    @include('sections.team')
-    
-    @include('sections.pricing')
-    
-    @include('sections.faq')
-    
-    @include('sections.contact')
+@include('sections.why_us')
+
+@include('sections.skills')
+
+@include('sections.services')
+
+@include('sections.cta')
+
+@include('sections.portfolio')
+
+@include('sections.team')
+
+@include('sections.pricing')
+
+@include('sections.faq')
+
+@include('sections.contact')
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,59 +1,62 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- Favicons -->
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Favicons -->
   <link href="{{ asset('img/favicon.png') }}" rel="icon">
   <link href="{{ asset('/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-       @include('layouts.header')
+  <div id="app">
+    @include('layouts.header')
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <main class="py-4">
+      @yield('content')
+    </main>
 
-        @include('layouts.footer')
+    @include('layouts.footer')
 
-  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
-  <div id="preloader"></div>
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    <div id="preloader"></div>
 
-         <!-- Vendor JS Files -->
-  <script src="{{ asset('js/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/php-email-form/validate.js') }}"></script>
-  <script src="{{ asset('js/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/venobox/venobox.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
-  <script src="{{ asset('js/vendor/aos/aos.js') }}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('js/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('js/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/venobox/venobox.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/aos/aos.js') }}"></script>
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('js/vendor/js/main.js') }}"></script>
+    <!-- Template Main JS File -->
+    <script src="{{ asset('js/vendor/js/main.js') }}"></script>
 
-    </div>
+  </div>
 </body>
+
 </html>
